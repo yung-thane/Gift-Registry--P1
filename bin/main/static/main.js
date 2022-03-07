780cd0006307498e347d70a02232e8ab770eb456
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-07 11:47:10 -0500

    :42 left 3/4, console.log is not working.

--- bin/main/static/main.js
+++ bin/main/static/main.js
@@ -1,3 +1,20 @@
 var welcomeMsg = 'Hello World!'
 
 document.querySelector('h1').innerText = welcomeMsg
+
+
+//fetch('/artists').then(resp => resp.json()).then(artists => console.log(artists));
+
+var req = new XMLHttpRequest();
+req.open('GET', '/artists');
+req.onload = function(){
+    if(req.status == 200){
+        let respText
+        console.log(req.responseText);
+    }
+}
+req.onerror = function(){
+    console.log('Oops');
+}
+req.send();
+
