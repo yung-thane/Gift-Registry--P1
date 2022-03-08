676a44fbc1db1a98fc484b40a8a28bac2e1005ee
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-07 19:47:50 -0500

    :23 left 3/4, NEED ISSUE RESOLVED, VERIFY IF PROGRAM IS WORKING

--- bin/main/static/main.js
+++ bin/main/static/main.js
@@ -3,18 +3,42 @@ var welcomeMsg = 'Hello World!'
 document.querySelector('h1').innerText = welcomeMsg
 
 
-//fetch('/artists').then(resp => resp.json()).then(artists => console.log(artists));
-
-var req = new XMLHttpRequest();
-req.open('GET', '/artists');
-req.onload = function(){
-    if(req.status == 200){
-        let respText
-        console.log(req.responseText);
-    }
+fetch('/artists').then(resp => resp.json()).then(artists => {
+    document.querySelector('body').innerHTML = listArtists(artists);
+});
+var listArtists = function(artist){
+    return '<p>' + artists.artistId +": " + artist.name + '</p>';
 }
-req.onerror = function(){
-    console.log('Oops');
-}
-req.send();
 
+
+function listArtists(json){
+    return `
+        <div id="artistsList">
+            ${json.map(listArtist).join('\n')}
+            </div>
+    `
+}
+
+// function get(url){
+//     return new Promise(function(resolve, reject){
+//         var req = new XMLHttpRequest();
+// req.open('GET', url);
+// req.onload = function(){
+//     if(req.status == 200){
+//         resolve(req.response);
+//     }else{
+//         reject(Error(req.statusText));
+//     }
+// }
+// req.onerror = function(){
+//     reject(Error("Network Error"));
+// }
+// req.send();
+//     });
+// }
+
+// get('/artists').then(function(response){
+//     console.log(JSON.parse(response));
+// }, function(err){
+
+// })
