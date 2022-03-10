fc173377e71363ffc2cc085914365949b4945187
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-10 12:15:04 -0500

    Added Post 1:16 3/7

--- bin/main/static/main.js
+++ bin/main/static/main.js
@@ -4,18 +4,39 @@ document.querySelector('h1').innerText = welcomeMsg;
 
 fetch('/items').then(resp => resp.json()).then(items => {
 document.querySelector('#items').innerHTML = listItems(items);
-}
+    }
 );
 
 let listItem = function(item){
     return '<p>' + item.itemId + ": " +item.name + '</p';
-}
+};
 
 
 let listItems = function(json){
-return `
-    <div id="itemsList">
-    ${json.map(listItem).join('\n')}
-    </div>
-`
+return `${json.map(listItem).join('\n')}`
+};
+
+function postItem(){
+    let item = {
+        "itemId": document.getElementById("itemId").value,
+        "name": document.getElementById("name").value
+    }
+    console.log(item);
+    fetch("/items", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    }).then((result) => {
+        if (result.status != 200) {
+            throw new Error("Bad Server Response");
+        }
+        console.log(result.text());
+    }).catch((error) => { console.log(error); })
+    fetch('/items').then(resp => resp.json()).then(items => {
+        document.querySelector('#items').innerHTML = listItems(items);
+        }
+    );
 }
--- src/main/java/com/revature/musica/App.java
+++ src/main/java/com/revature/musica/App.java
@@ -89,7 +89,7 @@ public class App {
                 System.out.println(newItem);
 
                 try {
-                    PreparedStatement stmt = connection.prepareStatement("insert into 'item' values(?, ?)");
+                    PreparedStatement stmt = connection.prepareStatement("insert into Item values(?, ?)");
                     stmt.setInt(1, newItem.getItemId());
                     stmt.setString(2, newItem.getName());
                     //Can't forget to execute statement, or it will just not happen.
