d309cf6e0f650efca68fad907afd9cdb0a067aa7
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-09 22:19:58 -0500

    FIXED WOW

--- bin/main/static/main.js
+++ bin/main/static/main.js
@@ -1,44 +1,21 @@
-var welcomeMsg = 'Welcome to the registry page!'
+var welcomeMsg = 'Welcome to the Registry!';
 
-document.querySelector('h1').innerText = welcomeMsg
+document.querySelector('h1').innerText = welcomeMsg;
 
 fetch('/items').then(resp => resp.json()).then(items => {
-        document.querySelector('body').innerHTML = listItems(items);
-    }
+document.querySelector('#items').innerHTML = listItems(items);
+}
 );
 
-let listItem = function(item) {
-    return '<p>' + item.itemId + ": " + item.name + '</p>';
+let listItem = function(item){
+    return '<p>' + item.itemId + ": " +item.name + '</p';
 }
 
-function listItems(json) {
-    return `
-        <div id="itemsList">
-            ${json.map(listItem).join('\n')}
-        </div>
-    `
+
+let listItems = function(json){
+return `
+    <div id="itemsList">
+    ${json.map(listItem).join('\n')}
+    </div>
+`
 }
-
-// function get(url) {
-//     return new Promise(function (resolve, reject) {
-//         var req = new XMLHttpRequest();
-//         req.open('GET', url);
-//         req.onload = function() {
-//             if (req.status == 200) {
-//                 resolve(req.response);
-//             } else {
-//                 reject(Error(req.statusText));
-//             }
-//         }
-//         req.onerror = function() {
-//             reject(Error("Network error"));
-//         }
-//         req.send();
-//     });
-// }
-
-// get('/artists').then(function(response) {
-//     console.log(JSON.parse(response));
-// }, function (err) {
-
-// })
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,3 +1,21 @@
-var welcomeMsg = 'Hello World!'
+var welcomeMsg = 'Welcome to the Registry!';
 
-document.querySelector('h1').innerText = welcomeMsg
+document.querySelector('h1').innerText = welcomeMsg;
+
+fetch('/items').then(resp => resp.json()).then(items => {
+document.querySelector('#items').innerHTML = listItems(items);
+}
+);
+
+let listItem = function(item){
+    return '<p>' + item.itemId + ": " +item.name + '</p';
+}
+
+
+let listItems = function(json){
+return `
+    <div id="itemsList">
+    ${json.map(listItem).join('\n')}
+    </div>
+`
+}
