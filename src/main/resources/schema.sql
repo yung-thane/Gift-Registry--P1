1a29e8d88af1b9d68607423e603ad4cf31c81ff8
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-14 18:53:55 -0400

    Favicon and background successfully changed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE "item" (
 
 CREATE TABLE "cart" (
     "CartId" INT NOT NULL,
-    "Title" VARCHAR NOT NULL,
+    "Name" VARCHAR NOT NULL,
     "ItemId" INT NOT NULL,
     CONSTRAINT "PK_Cart" PRIMARY KEY ("CartId"),
     CONSTRAINT "FK_CartItemId" FOREIGN KEY ("ItemId") REFERENCES "item" ("ItemId") ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -20,4 +20,4 @@ INSERT INTO "item" VALUES (5, 'Stroller');
 INSERT INTO "item" VALUES (6, 'Mobile');
 INSERT INTO "item" VALUES (7, 'Baby Camera');
 INSERT INTO "item" VALUES (8, 'Car Seat');
-INSERT INTO "cart" VALUES (1, 'Formula');
+INSERT INTO "cart" VALUES (1, 'Formula', 1);
