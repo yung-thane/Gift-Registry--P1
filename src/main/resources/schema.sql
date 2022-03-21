6f7562849ba06f4d5f6bd847874bc9b25e9fee67
Ronald <ronald.cristobal.lemus@outlook.com>
2022-03-21 08:26:46 -0400

    Same as last actual

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,4 +18,3 @@ INSERT INTO "item" VALUES (4, 'Stroller');
 INSERT INTO "item" VALUES (5, 'Mobile');
 INSERT INTO "item" VALUES (6, 'Baby Camera');
 INSERT INTO "item" VALUES (7, 'Car Seat');
-INSERT INTO "cart" VALUES (1, 'Formula', 1);
